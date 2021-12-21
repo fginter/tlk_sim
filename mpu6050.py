335168186af408b3ee0a2ff67f635542ff0517e7
Filip Ginter <ginter@cs.utu.fi>
2021-12-21 12:56:43 +0200

    latest changes

--- mpu6050.py
+++ mpu6050.py
@@ -6,6 +6,14 @@ from adafruit_bus_device.i2c_device import I2CDevice
 import json
 import busio
 
+import usb_hid
+import adafruit_hid
+
+import time
+import struct
+from adafruit_hid import find_device
+
+
 from time import sleep
 
 # deltat.py time difference calculation for sensor fusion
@@ -549,6 +557,141 @@ class Comb:
         self.smooth_roll=self.sum_of_rolls/len(self.rolls)
         self.smooth_pitch=self.sum_of_pitches/len(self.rolls)
 
+class Gamepad:
+    """Emulate a generic gamepad controller with 16 buttons,
+    numbered 1-16, and two joysticks, one controlling
+    ``x` and ``y`` values, and the other controlling ``z`` and
+    ``r_z`` (z rotation or ``Rz``) values.
+    The joystick values could be interpreted
+    differently by the receiving program: those are just the names used here.
+    The joystick values are in the range -127 to 127."""
+
+    def __init__(self, devices):
+        """Create a Gamepad object that will send USB gamepad HID reports.
+        Devices can be a list of devices that includes a gamepad device or a gamepad device
+        itself. A device is any object that implements ``send_report()``, ``usage_page`` and
+        ``usage``.
+        """
+        self._gamepad_device = find_device(devices, usage_page=0x1, usage=0x05)
+
+        # Reuse this bytearray to send mouse reports.
+        # Typically controllers start numbering buttons at 1 rather than 0.
+        # report[0] buttons 1-8 (LSB is button 1)
+        # report[1] buttons 9-16
+        # report[2] joystick 0 x: -127 to 127
+        # report[3] joystick 0 y: -127 to 127
+        # report[4] joystick 1 x: -127 to 127
+        # report[5] joystick 1 y: -127 to 127
+        self._report = bytearray(6)
+
+        # Remember the last report as well, so we can avoid sending
+        # duplicate reports.
+        self._last_report = bytearray(6)
+
+        # Store settings separately before putting into report. Saves code
+        # especially for buttons.
+        self._buttons_state = 0
+        self._joy_x = 0
+        self._joy_y = 0
+        self._joy_z = 0
+        self._joy_r_z = 0
+
+        # Send an initial report to test if HID device is ready.
+        # If not, wait a bit and try once more.
+        try:
+            self.reset_all()
+        except OSError:
+            time.sleep(1)
+            self.reset_all()
+
+    def press_buttons(self, *buttons):
+        """Press and hold the given buttons."""
+        for button in buttons:
+            self._buttons_state |= 1 << self._validate_button_number(button) - 1
+        self._send()
+
+    def release_buttons(self, *buttons):
+        """Release the given buttons."""
+        for button in buttons:
+            self._buttons_state &= ~(1 << self._validate_button_number(button) - 1)
+        self._send()
+
+    def release_all_buttons(self):
+        """Release all the buttons."""
+
+        self._buttons_state = 0
+        self._send()
+
+    def click_buttons(self, *buttons):
+        """Press and release the given buttons."""
+        self.press_buttons(*buttons)
+        self.release_buttons(*buttons)
+
+    def move_joysticks(self, x=None, y=None, z=None, r_z=None):
+        """Set and send the given joystick values.
+        The joysticks will remain set with the given values until changed
+        One joystick provides ``x`` and ``y`` values,
+        and the other provides ``z`` and ``r_z`` (z rotation).
+        Any values left as ``None`` will not be changed.
+        All values must be in the range -127 to 127 inclusive.
+        Examples::
+            # Change x and y values only.
+            gp.move_joysticks(x=100, y=-50)
+            # Reset all joystick values to center position.
+            gp.move_joysticks(0, 0, 0, 0)
+        """
+        if x is not None:
+            self._joy_x = self._validate_joystick_value(x)
+        if y is not None:
+            self._joy_y = self._validate_joystick_value(y)
+        if z is not None:
+            self._joy_z = self._validate_joystick_value(z)
+        if r_z is not None:
+            self._joy_r_z = self._validate_joystick_value(r_z)
+        self._send()
+
+    def reset_all(self):
+        """Release all buttons and set joysticks to zero."""
+        self._buttons_state = 0
+        self._joy_x = 0
+        self._joy_y = 0
+        self._joy_z = 0
+        self._joy_r_z = 0
+        self._send(always=True)
+
+    def _send(self, always=False):
+        """Send a report with all the existing settings.
+        If ``always`` is ``False`` (the default), send only if there have been changes.
+        """
+        struct.pack_into(
+            "<Hbbbb",
+            self._report,
+            0,
+            self._buttons_state,
+            self._joy_x,
+            self._joy_y,
+            self._joy_z,
+            self._joy_r_z,
+        )
+
+        if always or self._last_report != self._report:
+            self._gamepad_device.send_report(self._report)
+            # Remember what we sent, without allocating new storage.
+            self._last_report[:] = self._report
+
+    @staticmethod
+    def _validate_button_number(button):
+        if not 1 <= button <= 16:
+            raise ValueError("Button number must in range 1 to 16")
+        return button
+
+    @staticmethod
+    def _validate_joystick_value(value):
+        if not -127 <= value <= 127:
+            raise ValueError("Joystick value must be in range -127 to 127")
+        return value
+
+        
 # iic=busio.I2C(board.GP1,board.GP0)
 # while not iic.try_lock():
 #     pass
@@ -562,22 +705,26 @@ class Comb:
 c=Comb(board.GP9,board.GP8,100000) #scl,sda
 #c.calibrate()
 #print(c.calibration2string())
-c.calibrate_from_string("[[66.7707, -29.4564], [0.0279107, 0.0514151, -0.00060503], [-0.0478363, 0.00493958, 0.000115104]]")
+c.calibrate_from_string("[[65.657, -47.1609], [0.0261552, 0.0209381, -0.000537093], [-0.0359255, 0.00386623, 0.000278179]]")
+g=Gamepad(usb_hid.devices)
 
-
-#import adafruit_hid
-#from adafruit_hid.hid_gamepad import Gamepad
-#import adafruit_hid.gamepad
-#gp=adafruit_hid.gamepad.Gamepad()
 
 counter=0
 while True:
     c.ping()
-    if counter>=10:
-        #print(c.smooth_roll,c.smooth_pitch)
-        roll_01,pitch_01=c.position_01()
-        print("ROLL %03.1f"%(roll_01*100),"PITCH %03.1f"%(pitch_01*100),"     delta-t",c.fusion.deltat.last_dt)
-        counter=0
-    counter+=1
-    
+    roll_01,pitch_01=c.position_01()
+    try:
+        r,p=int(roll_01*255-127),int(pitch_01*255-127)
+        if r<-127:
+            r=-127
+        elif r>127:
+            r=127
+        if p<-127:
+            p=-127
+        elif p>127:
+            p=127
+        #print(r,p)
+        g.move_joysticks(r,-p)
+    except OSError:
+        pass
     
